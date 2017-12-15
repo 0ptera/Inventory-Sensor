2f6884bbc3c8f3487bb3c048758fd3b955e6d13a
Optera <yousei.kyoko+github@gmail.com>
2017-12-15 17:01:56 +0100

    1.6.1
    - updated graphics to match current constant combinator
    - added hr graphics

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -5,47 +5,44 @@ inv_sensor.icon_size = 32
 inv_sensor.minable.result = "item-sensor"
 
 inv_sensor.sprites =
-{
-  north =
-  {
-    filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
-    x = 158,
-    y = 5,
-    width = 79,
-    height = 63,
-    frame_count = 1,
-    shift = {0.140625, 0.140625},
-  },
-  east =
-  {
-    filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
-    y = 5,
-    width = 79,
-    height = 63,
-    frame_count = 1,
-    shift = {0.140625, 0.140625},
-  },
-  south =
-  {
-    filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
-    x = 237,
-    y = 5,
-    width = 79,
-    height = 63,
-    frame_count = 1,
-    shift = {0.140625, 0.140625},
-  },
-  west =
-  {
-    filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
-    x = 79,
-    y = 5,
-    width = 79,
-    height = 63,
-    frame_count = 1,
-    shift = {0.140625, 0.140625},
-  }
-}
+    make_4way_animation_from_spritesheet({ layers =
+      {
+        {
+          filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
+          width = 58,
+          height = 52,
+          frame_count = 1,
+          shift = util.by_pixel(0, 5),
+          hr_version =
+          {
+            scale = 0.5,
+            filename = "__Inventory Sensor__/graphics/entity/hr-inventory-sensor.png",
+            width = 114,
+            height = 102,
+            frame_count = 1,
+            shift = util.by_pixel(0, 5),
+          },
+        },
+        {
+          filename = "__base__/graphics/entity/combinator/constant-combinator-shadow.png",
+          width = 50,
+          height = 34,
+          frame_count = 1,
+          shift = util.by_pixel(9, 6),
+          draw_as_shadow = true,
+          hr_version =
+          {
+            scale = 0.5,
+            filename = "__base__/graphics/entity/combinator/hr-constant-combinator-shadow.png",
+            width = 98,
+            height = 66,
+            frame_count = 1,
+            shift = util.by_pixel(8.5, 5.5),
+            draw_as_shadow = true,
+          },
+        },
+      },
+    })
 
 inv_sensor.item_slot_count = 1000
     
