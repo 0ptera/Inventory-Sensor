bee53bffecc1009f27ea7d6458df5da2abf23f08
Henning Schmiedehausen <henning@schmiedehausen.org>
2024-11-15 22:07:57 -0800

    support factorio 2.0 (#28)
    * support factorio 2.0 by hgschmie

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -8,43 +8,25 @@ inv_sensor.sprites = make_4way_animation_from_spritesheet(
   { layers =
     {
       {
+        scale = 0.5,
         filename = "__Inventory Sensor__/graphics/entity/inventory-sensor.png",
-        width = 58,
-        height = 52,
+        width = 114,
+        height = 102,
         frame_count = 1,
         shift = util.by_pixel(0, 5),
-        hr_version =
-        {
-          scale = 0.5,
-          filename = "__Inventory Sensor__/graphics/entity/hr-inventory-sensor.png",
-          width = 114,
-          height = 102,
-          frame_count = 1,
-          shift = util.by_pixel(0, 5),
-        },
       },
       {
+        scale = 0.5,
         filename = "__base__/graphics/entity/combinator/constant-combinator-shadow.png",
-        width = 50,
-        height = 34,
+        width = 98,
+        height = 66,
         frame_count = 1,
-        shift = util.by_pixel(9, 6),
+        shift = util.by_pixel(8.5, 5.5),
         draw_as_shadow = true,
-        hr_version =
-        {
-          scale = 0.5,
-          filename = "__base__/graphics/entity/combinator/hr-constant-combinator-shadow.png",
-          width = 98,
-          height = 66,
-          frame_count = 1,
-          shift = util.by_pixel(8.5, 5.5),
-          draw_as_shadow = true,
-        },
       },
     },
   })
 
 inv_sensor.item_slot_count = 1000
-
 
 data:extend({ inv_sensor })
